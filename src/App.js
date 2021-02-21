cf71ec6dbd3a9a4506ea18b672962216f603e15d
vermashivam <vermashivam2510@gmail.com>
2021-02-21 22:39:03 +0530

    *launches

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route  , Switch} from 'react-router-dom';
+import AllLaunches from "./hoc/AllLaunches";
+import UpcomingLaunches from "./hoc/UpcomingLaunches";
+import PastLaunches from "./hoc/PastLaunches";
+
+import logo from './assets/Logo.png';
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src = {logo} alt = "spacex Logo"/>
       </header>
+      <Switch>
+        <Route exact path = '/' component = {AllLaunches} />
+        <Route exact path = '/upcoming' component = {UpcomingLaunches} />
+        <Route exact path = '/past' component = {PastLaunches} />
+      </Switch>
     </div>
   );
 }
